b084b3a380b5ba6e72f20950a020dff4f2bde0a5
Valerio Dandrea <dandrea@lfs1.mpi-hd.mpg.de>
2023-07-31 12:00:52 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# LEGEND Plot Blessing Plots
+
+Repo with all official plots ...
+
+In parallel to uploading the plot here, please also prepare a description of the plot on the dedicated Confluence page ...
+
+
+## LEGEND plot style:
+
+This repository also provides a small extension to the matplotlib library in order to have some consistent plotting style for LEGEND. You can install the extension using the following instructions:
+
+1. Download the githup repository using:
+```
+git clone .....
+```
+
+2. Install the package via:
+```
+pip install -e legend_plot_style/
+```
+
+3. Import the LEGEND plotting style via: 
+```
+from legend_plot_style import LEGENDPlotStyle
+# all other imports
+```
+WARNING: Please make sure that you import the `LEGENDPlotStyle` package first. If another package initalizes matplotlib first the extension wont work.
+
+After importing the extension you have access to the LEGEND plotting style and the LEGEND logo as watermark. You can add the water mark to a corresponding axes in the following way:
+```
+fix, axes = plt.subplots()
+axes.set_legend_logo('upper right')
+```
+By default the LEGEND dark logo is used. You can change the logo type, size and transparency via the provided keyword arguments. The size of the logo is automatically resized based on the figure size and the specified scaling_factor. Please do not hesistate to consult the python-help by pressing "shift" + "tab" or by calling `help(axes.set_legend_logo)` in your notebook for more information. 
---
+++ setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+# Get requirements from requirements.txt, stripping the version tags
+with open('requirements.txt') as f:
+    requires = [
+        r.split('/')[-1] if r.startswith('git+') else r
+        for r in f.read().splitlines()]
+
+with open('README.md') as file:
+    readme = file.read()
+
+setuptools.setup(name='legend_plot_style',
+                 version='0.0.1',
+                 description='plot settings for LEGEND',
+                 author='LEGEND collaboration',
+                 url='https://github.com/legend-exp/legend_plot_style',
+                 long_description=readme,
+                 long_description_content_type="text/markdown",
+                 setup_requires=['pytest-runner'],
+                 install_requires=requires,
+                 tests_require=requires + [
+                     'pytest',
+                 ],
+                 python_requires=">=3.6",
+                 packages=setuptools.find_packages(),
+                 scripts=[],
+                 classifiers=[
+                     'Development Status :: 4 - Beta',
+                     'License :: OSI Approved :: BSD License',
+                     'Natural Language :: English',
+                     'Programming Language :: Python :: 3.6',
+                     'Intended Audience :: Science/Research',
+                     'Programming Language :: Python :: Implementation :: CPython',
+                     'Topic :: Scientific/Engineering :: Physics',
+                 ],
+                 zip_safe=False)
